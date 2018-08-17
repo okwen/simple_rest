5e2ab5e86617f2efa77411546bbb43ba2dc011a9
Jarek Chiang <jarek@mail.jarek.tw>
2018-08-17 12:14:28 +0800

    修正 service/float.go 浮點數精度比對問題
    - 比對至小數點後第二位

--- service/float.go
+++ service/float.go
@@ -1,6 +1,20 @@
 package service
 
+import (
+	"fmt"
+	"strconv"
+)
+
+// 截取小数位数
+func FloatRound(f float64, n int) float64 {
+	format := "%." + strconv.Itoa(n) + "f"
+	res, _ := strconv.ParseFloat(fmt.Sprintf(format, f), 64)
+	return res
+}
+
 // MultiHundred : 傳入的數字乘 100 後回傳
 func MultiHundred(a float64) float64 {
-	return a * 100
+	qq := a * 100
+	qq = FloatRound(qq, 2)
+	return qq
 }
